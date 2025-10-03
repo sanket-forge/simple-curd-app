4762d80c0f3a577666ac6c9e182ce6c0e92884aa
sanket-forge <sanketkhatua9861@gmail.com>
2025-10-04 02:10:09 +0530

    small changes

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("hello hii")
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+
+app.listen(3000, () => {
+
+    console.log("Server is rinning on port 3000")
+})
+
+app.get('/', (req, res) => {
+
+    res.send("Hello from node api")
+})
+
+mongoose.connect('mongodb+srv://sanket_khatua:sanket@7064@backenddb.ysall21.mongodb.net/?retryWrites=true&w=majority&appName=backendDB')
+  .then(() => console.log('Connected!'))
+  .catch(() => console.log('Connection failed!'))
